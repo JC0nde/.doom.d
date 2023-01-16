30d5ede5e4386baf29b4fab19dda46724f086bef
JC0nde <mail@jonathanconde.com>
2023-01-16 08:45:37 +0100

    Add Organize Your Life In Plain Text! from Norang
    Problem at opening first file then everything ok!

---
+++ custom.el
@@ -0,0 +1,208 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ignored-local-variable-values
+   '((eval progn
+      (set
+       (make-local-variable 'org-time-clocksum-format)
+       '(:hours "%d" :require-hours t :minutes ":%02d" :require-minutes t))
+      (setq org-latex-tables-centered nil org-latex-default-table-environment "longtable")
+      (local-set-key
+       (kbd "<f6>")
+       (lambda nil
+         (interactive)
+         (beginning-of-buffer)
+         (re-search-forward "Invoice number: \\([0-9]+\\)")
+         (let
+             ((n
+               (string-to-number
+                (match-string 1))))
+           (kill-region
+            (match-beginning 1)
+            (match-end 1))
+           (insert
+            (format "%d"
+                    (1+ n))))
+         (beginning-of-buffer)
+         (re-search-forward "Invoice date: *")
+         (kill-region
+          (point)
+          (save-excursion
+            (end-of-line)
+            (point)))
+         (org-insert-time-stamp
+          (current-time)
+          nil t)
+         (beginning-of-buffer)
+         (search-forward "#+BEGIN: clocktable")
+         (unwind-protect
+             (progn
+               (defadvice org-table-goto-column
+                   (before always-make-new-columns
+                           (n &optional on-delim force)
+                           activate)
+                 "always adds new columns when we move to them"
+                 (setq force t))
+               (org-clocktable-shift 'right 1))
+           (ad-deactivate 'org-table-goto-column))
+         (beginning-of-buffer)
+         (search-forward "| totaltarget")
+         (org-table-recalculate t))))
+     (eval progn
+      (set
+       (make-local-variable 'org-time-clocksum-format)
+       '(:hours "%d" :require-hours t :minutes ":%02d" :require-minutes t))
+      (setq org-latex-tables-centered nil org-latex-default-table-environment "longtable")
+      (local-set-key
+       (kbd "<f6>")
+       (lambda nil
+         (interactive)
+         (beginning-of-buffer)
+         (re-search-forward "Facture numéro: \\([0-9]+\\)")
+         (let
+             ((n
+               (string-to-number
+                (match-string 1))))
+           (kill-region
+            (match-beginning 1)
+            (match-end 1))
+           (insert
+            (format "%d"
+                    (1+ n))))
+         (beginning-of-buffer)
+         (re-search-forward "Date de facturation: *")
+         (kill-region
+          (point)
+          (save-excursion
+            (end-of-line)
+            (point)))
+         (org-insert-time-stamp
+          (current-time)
+          nil t)
+         (beginning-of-buffer)
+         (search-forward "#+BEGIN: clocktable")
+         (unwind-protect
+             (progn
+               (defadvice org-table-goto-column
+                   (before always-make-new-columns
+                           (n &optional on-delim force)
+                           activate)
+                 "always adds new columns when we move to them"
+                 (setq force t))
+               (org-clocktable-shift 'right 1))
+           (ad-deactivate 'org-table-goto-column))
+         (beginning-of-buffer)
+         (search-forward "| totaltarget")
+         (org-table-recalculate t))))))
+ '(org-agenda-files
+   '("/home/jonathan/Org/todo.org" "/home/jonathan/Org/clients/fondation-taurus/fondation-taurus.org" "/home/jonathan/Org/website.org" "/home/jonathan/Org/refile.org" "/home/jonathan/Org/pihole/pihole.org" "/home/jonathan/Org/org-mode.org" "/home/jonathan/Org/independant.org" "/home/jonathan/Org/diary.org" "/home/jonathan/Org/clients/actionpourmalades/actionpourmalades.org" "/home/jonathan/Org/clients/demart/demart.org" "/home/jonathan/Org/clients/fondation-taurus/fondationtaurus-chaisesvolantes.org"))
+ '(safe-local-variable-values
+   '((eval progn
+      (set
+       (make-local-variable 'org-time-clocksum-format)
+       '(:hours "%d" :require-hours t :minutes ":%02d" :require-minutes t))
+      (setq org-latex-tables-centered nil org-latex-default-table-environment "longtable")
+      (local-set-key
+       (kbd "<f6>")
+       (lambda nil
+         (interactive)
+         (beginning-of-buffer)
+         (re-search-forward "Invoice number: \\([0-9]+\\)")
+         (let
+             ((n
+               (string-to-number
+                (match-string 1))))
+           (kill-region
+            (match-beginning 1)
+            (match-end 1))
+           (insert
+            (format "%d"
+                    (1+ n))))
+         (beginning-of-buffer)
+         (re-search-forward "Invoice date: *")
+         (kill-region
+          (point)
+          (save-excursion
+            (end-of-line)
+            (point)))
+         (org-insert-time-stamp
+          (current-time)
+          nil t)
+         (beginning-of-buffer)
+         (search-forward "#+BEGIN: clocktable")
+         (unwind-protect
+             (progn
+               (defadvice org-table-goto-column
+                   (before always-make-new-columns
+                           (n &optional on-delim force)
+                           activate)
+                 "always adds new columns when we move to them"
+                 (setq force t))
+               (org-clocktable-shift 'right 1))
+           (ad-deactivate 'org-table-goto-column))
+         (beginning-of-buffer)
+         (search-forward "| totaltarget")
+         (org-table-recalculate t))))
+     (eval progn
+      (set
+       (make-local-variable 'org-time-clocksum-format)
+       '(:hours "%d" :require-hours t :minutes ":%02d" :require-minutes t))
+      (setq org-latex-tables-centered nil org-latex-default-table-environment "longtable")
+      (local-set-key
+       (kbd "<f6>")
+       (lambda nil
+         (interactive)
+         (beginning-of-buffer)
+         (re-search-forward "Facture numéro: \\([0-9]+\\)")
+         (let
+             ((n
+               (string-to-number
+                (match-string 1))))
+           (kill-region
+            (match-beginning 1)
+            (match-end 1))
+           (insert
+            (format "%d"
+                    (1+ n))))
+         (beginning-of-buffer)
+         (re-search-forward "Date de facturation: *")
+         (kill-region
+          (point)
+          (save-excursion
+            (end-of-line)
+            (point)))
+         (org-insert-time-stamp
+          (current-time)
+          nil t)
+         (beginning-of-buffer)
+         (search-forward "#+BEGIN: clocktable")
+         (unwind-protect
+             (progn
+               (defadvice org-table-goto-column
+                   (before always-make-new-columns
+                           (n &optional on-delim force)
+                           activate)
+                 "always adds new columns when we move to them"
+                 (setq force t))
+               (org-clocktable-shift 'right 1))
+           (ad-deactivate 'org-table-goto-column))
+         (beginning-of-buffer)
+         (search-forward "| totaltarget")
+         (org-table-recalculate t))))))
+ '(warning-suppress-types
+   '((initialization)
+     (initialization)
+     (initialization)
+     (initialization)
+     (initialization)
+     (defvaralias)) t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(font-lock-comment-face ((t (:slant italic))))
+ '(font-lock-keyword-face ((t (:slant italic))))
+ '(org-mode-line-clock ((t (:foreground "red" :box (:line-width -1 :style released-button))))))
